b2909f25c4ea6265f8b3d5137355612f5030298b
qurtayX <89860028+qurtayX@users.noreply.github.com>
2022-03-18 00:36:10 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# muzk-bot
+# Discord.js v13 Müzik Botu
+Umut Bayraktar Youtube Özel Discord.js v13 Müzik Botu Altyapısı.<br>
+<b>.env</b> İsimli Dosyaya Bot Tokeninizi Yazın ve Kullanın.<br>
+<br><br>
+Umut Bayraktar Youtube: <a href="https://www.youtube.com/UmutBayraktarYT">Abone Ol</a><br>
+Discord: <a href="https://discord.gg/58e5H4try3">Katıl</a>
+<br><br>
+Code Share Discord: <a href="https://discord.gg/6XGqdgE">KATIL</a>
---
+++ config.js
@@ -0,0 +1,20 @@
+module.exports = {
+        px: '!',
+        playing: '🤙🏾 NİLOYA ',
+
+    opt: {
+        DJ: {
+            enabled: false, //EĞER SADECE DJLER KULLANA BİLSİN İSTİYOR İSENİZ false yazanı true yapın.
+            roleName: 'DJ', //DJ ROLÜNÜN İSMİ NE OLACAK İSE YAZIN SUNUCUNUZDA O ROLDEKİLER KULLANA BİLİR
+            commands: ['back', 'clear', 'filter', 'loop', 'pause', 'resume', 'skip', 'stop', 'volume'] //DOKUNMA
+        },
+        maxVol: 250, //maximum ses seviyesi kaç olacak belirte bilirsiniz.
+        loopMessage: false, //elleme
+        discordPlayer: {
+            ytdlOptions: {
+                quality: 'highestaudio', //elleme
+                highWaterMark: 1 << 25 //elleme
+            }
+        }
+    }
+};
